1e0ab2f32966afeb7a30d70400a4e004d8dec964
p-bartl <pascal.bartl@bmw.de>
2021-11-19 13:31:54 +0000

    Initial Commit

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ZooComponent } from './zoo/zoo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ZooComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
